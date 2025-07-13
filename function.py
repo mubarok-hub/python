8c85d06dd84a86d11053de00d430103ce912e657
mubarok-hub <ridjzal24@gmail.com>
2025-07-13 13:34:20 -0300

    nambah

--- function.py
+++ function.py
@@ -2,3 +2,19 @@ def halo(nama):
     return f"Halo, {nama}!"
 
 print(halo("alin"))
+
+def tambah(a, b):
+    return a + b
+
+hasil = tambah(3, 5)
+print('hasil:', hasil)
+
+def cek_nilai(nilai):
+    if nilai >= 90:
+        return "A"
+    elif nilai >= 75:
+        return "B"
+    else:
+        return "C"
+
+print(cek_nilai(85))  # Output: B
--- perulangan.py
+++ perulangan.py
@@ -1,5 +1,9 @@
 # for
-for i  in renge(5):
+buah = ['apel', 'jeruk', 'mangga', 'pisang']
+for item in buah:
+    print('saya suka', item)
+
+for i in range(5):
     print("loop ke-", i )
 
 # while
@@ -7,3 +11,9 @@ n = 0
 while n < 3:
     print("n =", n)
     n += 1
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i, 'adalaha genap')
+    else:
+        print(i, 'adalah ganjil')
